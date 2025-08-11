931d45de98b2ffe045dd53f85cdf03030cd9b375
FrancoisBasset <mrfrancoisbasset@gmail.com>
2025-08-11 16:39:36 +0200

    feat(security): use plain md5

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -5,14 +5,12 @@ namespace App\Controller;
 use App\Repository\PokemonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 final class PokemonController extends AbstractController
 {
     #[Route('/pokemon', name: 'pokemon')]
-    public function index(PokemonRepository $pokemonRepository, SerializerInterface $serializer): JsonResponse
+    public function index(PokemonRepository $pokemonRepository): JsonResponse
     {
         return $this->json($pokemonRepository->findAll(), 200, [], ['groups' => 'pokemon:list']);
     }
---
+++ src/Security/Md5PasswordHasher.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+
+class Md5PasswordHasher implements PasswordHasherInterface
+{
+    public function hash(string $plainPassword): string
+    {
+        return md5($plainPassword);
+    }
+
+    public function verify(string $hashedPassword, string $plainPassword): bool
+    {
+        return hash_equals($hashedPassword, md5($plainPassword));
+    }
+
+    public function needsRehash(string $hashedPassword): bool
+    {
+        return false;
+    }
+}
